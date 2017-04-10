fcbdc934ee01bb29f8cc474e1be7b5ce73ce4f91
XiaoQ <1692303173@qq.com>
2017-04-10 09:42:27 +0800

    first commit

---
+++ app/src/main/java/com/xiaoq/android_soundrecording/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xiaoq.android_soundrecording;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
